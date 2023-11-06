21f883d2f6287b12c98aeda2da4ab025e8f48e73
kapilparsodkar <95988187+kapilparsodkar@users.noreply.github.com>
2023-11-06 17:05:17 -0500

    express mvc

--- express mvc/controllers/messages.controller.js
+++ express mvc/controllers/messages.controller.js
@@ -1,9 +1,16 @@
+const path = require("path");
+
 function postmessages(req, res) {
   console.log("updating messages");
 }
 
 function getmessages(req, res) {
-  res.send("<ul><li>hello kapil</li></ul>");
+  res.render("messages", {
+    title: "Message to you",
+    friend: "kaps",
+  });
+  // res.sendFile(path.join(__dirname, "..", "public", "images", "kaps.jpg"));
+  //res.send("<ul><li>hello kapil</li></ul>");
 }
 
 module.exports = {
--- express mvc/index.js
+++ express mvc/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const friendRouter = require("./Routes/friends.router.js");
 const MessageRouter = require("./Routes/messages.router");
 const app = express();
+
 const hostname = "127.0.0.1";
 const port = 3000;
-
+const path = require("path");
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 //middleware take a request then by next it sends to the method which is in case get method friends so it calculates time taken for response to come from friends as start notes some time then next goes into friends method response comes from their then current time is subtracted from start time to calculate time of responsee
 app.use((req, res, next) => {
   const start = Date.now();
@@ -13,11 +16,19 @@ app.use((req, res, next) => {
   const delta = Date.now() - start;
   console.log(`${req.method} ${req.url} ${delta}ms`);
 });
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "tiger",
+    caption: "tigers are biggest cat",
+  });
+});
 
 app.use("/friends", friendRouter);
 
